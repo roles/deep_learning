d5add176e501b1b05eba1b92059b0b34cf02eeb4
roles <roles568@gmail.com>
2014-12-10 16:02:38 +0800

    deepclassrbm

--- src/DeepClassRBM.cpp
+++ src/DeepClassRBM.cpp
@@ -56,7 +56,7 @@ void testDeepClassRBMTraining(){
     MNISTDataset data;
     data.loadData();
     
-    MultiLayerRBM* multirbm = new MultiLayerRBM("MNISTMultiLayerRBM_1000_1000_1000_0.01.dat");
+    MultiLayerRBM* multirbm = new MultiLayerRBM("result/MNISTMultiLayerRBM_1000_1000_1000_0.01.dat");
     multirbm->setLayerToTrain(0, false);
     multirbm->setLayerToTrain(1, false);
     multirbm->setLayerToTrain(2, false);
